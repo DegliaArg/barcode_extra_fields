1867bf3f586d2b98b89d999af1a4d08ef0a60ef0
PCerutti <pablocerutti@deglia.xyz>
2026-01-06 11:13:18 -0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Barcode – Campos extra",
+    "version": "18.0.1.0.0",
+    "depends": [
+        "stock",
+        "stock_barcode",
+    ],
+    "data": [
+        "views/barcode_templates.xml",
+    ],
+    "installable": True,
+}
