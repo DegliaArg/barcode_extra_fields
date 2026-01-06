2b9440dec368a3c12861b458715335c0884a641a
PCerutti <pablocerutti@deglia.xyz>
2026-01-06 14:55:25 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,10 @@
 {
-    "name": "Barcode Custom Demanda",
-    "version": "16.0.1.0.0",
+    "name": "Barcode Extra Fields",
+    "version": "1.0",
     "depends": ["stock_barcode"],
     "data": [
         "views/barcode_templates.xml",
     ],
     "installable": True,
-    "auto_install": False,
     "application": False,
 }
